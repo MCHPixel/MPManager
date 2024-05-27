907933217ba149d5f1b868c20f5a53869db43886
MCHPixel <schulzefabian@outlook.de>
2024-05-28 01:39:55 +0200

    Moved the Executions to another file

--- src/main/java/mchpixel/mpmanager/MPManager.java
+++ src/main/java/mchpixel/mpmanager/MPManager.java
@@ -1,9 +1,6 @@
 package mchpixel.mpmanager;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
+import mchpixel.mpmanager.features.joinmessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MPManager extends JavaPlugin {
@@ -11,23 +8,11 @@ public final class MPManager extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new joinmessage(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Don't open auction house here
-        Player player = event.getPlayer();
-
-        String pref = ChatColor.stripColor(player.getDisplayName());
-
-        player.sendMessage(ChatColor.WHITE + pref + "Welcome to the Server!");
-
-        player.sendTitle(ChatColor.GOLD + "Welcome to MCHPixel", player.getName(), 20, 60, 20);
     }
 }
--- src/main/java/mchpixel/mpmanager/features/joinmessage.java
+++ src/main/java/mchpixel/mpmanager/features/joinmessage.java
@@ -3,20 +3,21 @@ package mchpixel.mpmanager.features;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class joinmessage {
+public class joinmessage implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         // Don't open auction house here
         Player player = event.getPlayer();
 
-        String pref = ChatColor.stripColor(player.getDisplayName());
+        String pref = ChatColor.stripColor(player.getName());
 
-        player.sendMessage(ChatColor.WHITE + pref + "Welcome to the Server!");
+        player.sendMessage(ChatColor.WHITE + pref + " Welcome to the Server!");
 
-        player.sendTitle(ChatColor.GOLD + "Welcome to MCHPixel", player.getName(), 20, 60, 20);
+        player.sendTitle(ChatColor.GOLD + "Welcome to MCHPixel", player.getName(), 20, 100, 20);
     }
 
 }
